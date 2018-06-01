249e3cbbb42a6fe86148d1d412ccbceebf3b89cb
carlosbleycher <monteffiori@gmail.com>
2018-06-01 17:45:03 +0200

    Ver. 2018

--- includes/form_29134.php
+++ includes/form_29134.php
@@ -9,7 +9,7 @@
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from Final.";
+	$email_subject = "Message from Final_2018.";
 	$email_body = "You have received a new message. \n\n".
 				  ": $ \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
